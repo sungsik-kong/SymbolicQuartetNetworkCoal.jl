3721b73aac16c5b94bc2137c1250e0b6b34cc31d
eallman <e.allman@alaska.edu>
2025-10-26 11:27:16 -0800

    changing matlab output slightly

--- src/reformatting.jl
+++ src/reformatting.jl
@@ -61,25 +61,25 @@ function reformat_export(net, df;
             str="% Declare variables\n"
             str=str*"syms "
             for par in params str=str*par*" " end
-            for i in 1:numCFs str=str*"C_$i " end
+            for i in 1:numCFs str=str*"C$i " end
             str=str*"\n\n% matrix of generating polynomials\n"
             str=str*"F=["
             i=1
             while i<numCFs
                 str1=(dataframe[i,2])
                 str1=replace(str1,string(inheritancecorrelation)=>rationalize(inheritancecorrelation))                
-                str=str*"$(str1)-C_$i,\n"
+                str=str*"$(str1)-C$i,\n"
                 i+=1
             end
             str1=(dataframe[numCFs,2])
             str1=replace(str1,string(inheritancecorrelation)=>rationalize(inheritancecorrelation))            
-            str=str*"$(str1)-C_$numCFs];\n"
+            str=str*"$(str1)-C$numCFs];\n"
             str=str*"\n% matrix of generating polynomials\n"
             str=str*"\n% Array of all variables\n"
             str=str*"V=["
             for par in params str=str*par*" " end
-            for i in 1:numCFs-1 str=str*"C_$i " end
-            str=str*"C_$numCFs]\n"
+            for i in 1:numCFs-1 str=str*"C$i " end
+            str=str*"C$numCFs]\n"
             str=str*"\n% Compute dimension\nCoalDim(F,V)"
             write(file, str)
         end
