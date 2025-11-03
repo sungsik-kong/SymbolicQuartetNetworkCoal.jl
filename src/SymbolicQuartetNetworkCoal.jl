cac98a6dc788a380f9e84d6554e08dbe91a3149b
Sungsik Kevin Kong <sungsik.kong@gmail.com>
2025-11-03 12:43:46 +0900

    Update SymbolicQuartetNetworkCoal.jl

--- src/SymbolicQuartetNetworkCoal.jl
+++ src/SymbolicQuartetNetworkCoal.jl
@@ -1,28 +1,28 @@
 module SymbolicQuartetNetworkCoal
-using CSV
-using DataFrames
-#using Dates
-using PhyloNetworks
-using StaticArrays
-import Random
 
-const dpoints=10 # decimal points for all parameters when randomly generated
-const eLab="t_"
-const gLab="g_"
-const PN = PhyloNetworks
+    using CSV
+    using DataFrames
+    #using Dates
+    using PhyloNetworks
+    using StaticArrays
+    import Random
 
-export
-aloha,
-export_csv,
-export_symbolic_format,
-readTopologyrand,
-network_expectedCF_formulas,
-# makeEdgeLabel_v3,
-makeEdgeLabel,
-assignBinaryEdgeLengths
+    const dpoints=10 # decimal points for all parameters when randomly generated
+    const eLab="t_"
+    const gLab="g_"
+    const PN = PhyloNetworks
 
-include("misc.jl")
-include("symbolicCF.jl")
-include("formatting.jl")    
+    export
+    aloha,
+    export_csv,
+    export_symbolic_format,
+    readTopologyrand,
+    network_expectedCF_formulas,
+    makeEdgeLabel,
+    assignBinaryEdgeLengths
+
+    include("misc.jl")
+    include("symbolicCF.jl")
+    include("formatting.jl")    
 
 end # module
